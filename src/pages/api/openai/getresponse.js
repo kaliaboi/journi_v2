dfe7825d043e0768a1581b52666abe5e82f1ae6e
designdevhybrid <abhishekkalia.productdesign@gmail.com>
2023-06-03 16:33:27 -0400

    Add openai lib

--- src/pages/api/openai/getresponse.js
+++ src/pages/api/openai/getresponse.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     model: "gpt-3.5-turbo",
     messages: [...initialMessages, { role: "user", content: "I am sad" }],
   });
-  console.log(completion.data.choices[0].message.content);
+  console.log("completion", completion.data.choices[0].message.content);
   res
     .status(200)
     .json({ response: completion.data.choices[0].message.content });
